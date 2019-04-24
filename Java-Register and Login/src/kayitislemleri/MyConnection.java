51a0449e19339d6d983b37ac203daa16db808f8d
Utku Kemal Çifçi <utku33.37@hotmail.com>
2019-04-24 11:36:25 +0300

    Login ve Register

---
+++ Java-Register and Login/src/kayitislemleri/MyConnection.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kayitislemleri;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Utku Kemal
+ */
+public class MyConnection {
+    public static Connection getConnection(){
+        Connection con=null;
+         try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con=DriverManager.getConnection("jdbc:mysql://localhost/loginandregister","root","");
+                
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            
+        }
+        return con;
+    }
+    
+}
